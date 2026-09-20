21049a3135bb5f3797c2efc7132cff0eedc6ce58
Sergio Cordero Calvimontes <sergio.cordero.calvimontes@gmail.com>
2026-09-20 03:32:55 -0300

    firmware: encapsulate AVR direct-port fast paths

---
+++ avr_fast_io.h
@@ -0,0 +1,108 @@
+#ifndef AVR_FAST_IO_H
+#define AVR_FAST_IO_H
+
+#include <Arduino.h>
+
+#include "project_config.h"
+#include "avr_contracts.h"
+
+
+namespace AvrFastIo {
+
+
+inline void toggleBlueLed(
+    uint8_t index) {
+
+  const uint8_t pin =
+      (uint8_t)(
+          Config::BLUE_LED_FIRST_PIN +
+          index);
+
+
+#if defined(__AVR_ATmega328P__)
+
+  PORTD ^=
+      (uint8_t)(
+          1U <<
+          (AvrContracts::BLUE_LED_PORT_FIRST_BIT + index));
+
+#else
+
+  digitalWrite(
+      pin,
+      digitalRead(pin)
+          ? LOW
+          : HIGH);
+
+#endif
+}
+
+
+inline void sampleButtons(
+    bool &mainPressed,
+    bool &decreasePressed,
+    bool &increasePressed) {
+
+#if defined(__AVR_ATmega328P__)
+
+  const uint8_t pinc =
+      PINC;
+
+
+  mainPressed =
+      !(pinc &
+        (1U << AvrContracts::MAIN_BUTTON_PORT_BIT));
+
+  decreasePressed =
+      !(pinc &
+        (1U << AvrContracts::DECREASE_BUTTON_PORT_BIT));
+
+  increasePressed =
+      !(pinc &
+        (1U << AvrContracts::INCREASE_BUTTON_PORT_BIT));
+
+#else
+
+  mainPressed =
+      digitalRead(
+          Config::MAIN_BUTTON_PIN) == LOW;
+
+  decreasePressed =
+      digitalRead(
+          Config::DECREASE_INTERVAL_BUTTON_PIN) == LOW;
+
+  increasePressed =
+      digitalRead(
+          Config::INCREASE_INTERVAL_BUTTON_PIN) == LOW;
+
+#endif
+}
+
+
+inline void toggleSchedulerHeartbeat() {
+
+#if defined(__AVR_ATmega328P__)
+
+  PORTC ^=
+      (uint8_t)(
+          1U <<
+          AvrContracts::HEARTBEAT_PORT_BIT);
+
+#else
+
+  digitalWrite(
+      Config::SCHEDULER_HEARTBEAT_LED_PIN,
+
+      digitalRead(
+          Config::SCHEDULER_HEARTBEAT_LED_PIN)
+          ? LOW
+          : HIGH);
+
+#endif
+}
+
+
+}  // namespace AvrFastIo
+
+
+#endif
